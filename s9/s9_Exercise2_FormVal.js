edf266d85c03dbe13cd347c5a43dcaa529b82a11
Abhijeet Chaudhari <abhichaudhari@gmail.com>
2018-08-20 20:54:30 -0400

    Committing correct version of s9_Exercise2_FormVal.js

--- s9/s9_Exercise2_FormVal.js
+++ s9/s9_Exercise2_FormVal.js
@@ -1,5 +1,25 @@
-let required = ['name', 'email', 'phone'];
+let required = ['name', 'email', 'phone']
 
 $(document).ready(function () {
     $("input#submit").on('click', validateForm)
 })
+function validateForm(event) {
+    event.preventDefault()
+    let message = $("p#message")
+    let invalid = false
+    required.forEach(function (id) {
+        let input = $(`input#${id}`)
+        let label = input.prev("label")
+        if (input.val() == "") {
+            label.addClass("warning")
+            invalid = true
+        } else {
+            label.removeClass("warning")
+        }
+    })
+    if (invalid) {
+        message.html("Please Fill Out Required Fields")
+    } else {
+        $("#pre-form h2").html("Thanks for your feedback!")
+    }
+}
